835ff9cc4cc0ceb35362a4f0ef75370e23e90fd7
mazharrasyad <muhazharrasyad@gmail.com>
2020-11-11 12:20:00 +0700

    update heroku

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -8,6 +8,7 @@ use App\User;
 class ApiController extends Controller
 {
     public function get_all_user(){
-        return response()->json(User::all(), 200);
+        // return response()->json(User::all(), 200);
+        return array('results' => User::all());
     }
 }
